56226c070481b40953113822eb3340a3c2748b7d
ysmaraujo <yasminnaraujo@hotmail.com>
2024-06-11 10:01:24 +0100

    Initial Commit

---
+++ RockPaperScissors./RockPaperScissors_App.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissors_App.swift
+//  RockPaperScissors.
+//
+//  Created by Yasmin araujo on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissors_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
